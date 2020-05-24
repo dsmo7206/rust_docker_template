c4710ba59fc3a31c32ab66ab69a0cb1c2fb64b9a
David Smoker <dsmoker@gmail.com>
2020-05-24 19:45:28 +0100

    Upgraded lib versions; minor client change

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,6 @@
+use tokio::time::delay_for;
+use std::time::Duration;
+
 use echo_package::echo_service_client::EchoServiceClient;
 use echo_package::EchoRequest;
 
@@ -10,17 +13,19 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Client connecting...");
     let mut client = EchoServiceClient::connect("http://server:50051").await?;
 
-    println!("Client sending request...");
+    loop {
+        println!("Client sending request...");
 
-    let request = tonic::Request::new(
-        EchoRequest {
-            message: "Test!".into()
-        }
-    );
+        let request = tonic::Request::new(
+            EchoRequest {
+                message: "Test!".into()
+            }
+        );
+        let response = client.echo(request).await?;
 
-    let response = client.echo(request).await?;
-
-    println!("Client got response: {:?}", response);
+        println!("Client got response: {:?}", response);
+        delay_for(Duration::from_millis(1000)).await;
+    }
 
     Ok(())
 }
