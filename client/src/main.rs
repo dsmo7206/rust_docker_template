6e7979d6789c097c82dd8a6d98c5e5d7b9d3dc39
David Smoker <dsmoker@gmail.com>
2020-05-24 17:34:28 +0100

    Updates

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,8 +1,8 @@
-use rust_backend::echo_service_client::EchoServiceClient;
-use rust_backend::EchoRequest;
+use echo_package::echo_service_client::EchoServiceClient;
+use echo_package::EchoRequest;
 
-pub mod rust_backend {
-    tonic::include_proto!("rust_backend");
+pub mod echo_package {
+    tonic::include_proto!("echo_package");
 }
 
 #[tokio::main]
@@ -10,20 +10,17 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Client connecting...");
     let mut client = EchoServiceClient::connect("http://server:50051").await?;
 
-    loop {
-        println!("Client sending request...");
+    println!("Client sending request...");
 
-        let request = tonic::Request::new(
-            EchoRequest {
-                message: "Test!".into()
-            }
-        );
+    let request = tonic::Request::new(
+        EchoRequest {
+            message: "Test!".into()
+        }
+    );
 
-        let response = client.echo(request).await?;
+    let response = client.echo(request).await?;
 
-        println!("Client got response: {:?}", response);
-        std::thread::sleep_ms(1000);
-    }
+    println!("Client got response: {:?}", response);
 
     Ok(())
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,21 +1,22 @@
 use tonic::{transport::Server, Request, Response, Status};
-use rust_backend::echo_service_server::{EchoService, EchoServiceServer};
-use rust_backend::{EchoRequest, EchoResponse};
+use echo_package::echo_service_server::{EchoService, EchoServiceServer};
+use echo_package::{EchoRequest, EchoResponse};
 
-pub mod rust_backend {
-    tonic::include_proto!("rust_backend");
+const USE_IPV6: bool = false;
+const PORT: u16 = 50051;
+
+pub mod echo_package {
+    tonic::include_proto!("echo_package");
 }
 
 #[derive(Debug, Default)]
-pub struct MyEchoService {
-    i: u32
-}
+pub struct MyEchoService {}
 
 #[tonic::async_trait]
 impl EchoService for MyEchoService {
     async fn echo(&self, request: Request<EchoRequest>) -> Result<Response<EchoResponse>, Status> {
         let message = request.into_inner().message;
-        let reply = rust_backend::EchoResponse {
+        let reply = echo_package::EchoResponse {
             message: format!("Hello {}", message)
         };
         Ok(Response::new(reply))
@@ -24,8 +25,11 @@ impl EchoService for MyEchoService {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    //let address = "[::1]:50051".parse()?;
-    let address = "0.0.0.0:50051".parse()?;
+    let address = {
+        if USE_IPV6 { format!("[::1]:{}", PORT) }
+        else { format!("0.0.0.0:{}", PORT) }
+    }.parse()?;
+
     let echo_service = MyEchoService::default();
 
     println!("Server running...");
